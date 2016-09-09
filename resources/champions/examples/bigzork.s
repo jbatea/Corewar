045cb3f6a41255a6445542696f197662cb6a4577
Johann BATEAU <jbateau@e3r10p21.42.fr>
2016-09-09 10:08:04 +0200

    Added champions

--- resources/champions/examples/bigzork.s
+++ resources/champions/examples/bigzork.s
@@ -1,90 +1,91 @@
 .name "bigzork"
 .comment "I know i cant win"
 
-ld %0, r2
-st r1, 211
-st r1, 211
-st r1, 211
-st r1, 211
-st r1, 211
-st r1, 211
-st r1, 211
-st r1, 211
-st r1, 211
-st r1, 211
-st r1, 211
-st r1, 211
-st r1, 211
-st r1, 211
-st r1, 211
-st r1, 211
-st r1, 211
-st r1, 211
-st r1, 211
-st r1, 211
-st r1, 211
-st r1, 211
-st r1, 211
-st r1, 211
-st r1, 211
-st r1, 211
-st r1, 211
-st r1, 211
-st r1, 211
-st r1, 211
-st r1, 211
-st r1, 211
-st r1, 211
-st r1, 211
-st r1, 211
-st r1, 211
-st r1, 211
-st r1, 211
-st r1, 211
-st r1, 211
-st r1, 211
-st r1, 211
-loop:
-live %1
-live %1
-live %1
-live %1
-live %1
-live %1
-live %1
-live %1
-live %1
-live %1
-live %1
-live %1
-live %1
-live %1
-live %1
-live %1
-live %1
-live %1
-live %1
-live %1
-live %1
-live %1
-live %1
-live %1
-live %1
-live %1
-live %1
-live %1
-live %1
-live %1
-live %1
-live %1
-live %1
-live %1
-live %1
-live %1
-live %1
-live %1
-live %1
-live %1
-live %1
-live %1
-zjmp %:loop
+# INDICATED SIZE : 430
+
+ld	%0, r2
+st	r1, 211
+st	r1, 211
+st	r1, 211
+st	r1, 211
+st	r1, 211
+st	r1, 211
+st	r1, 211
+st	r1, 211
+st	r1, 211
+st	r1, 211
+st	r1, 211
+st	r1, 211
+st	r1, 211
+st	r1, 211
+st	r1, 211
+st	r1, 211
+st	r1, 211
+st	r1, 211
+st	r1, 211
+st	r1, 211
+st	r1, 211
+st	r1, 211
+st	r1, 211
+st	r1, 211
+st	r1, 211
+st	r1, 211
+st	r1, 211
+st	r1, 211
+st	r1, 211
+st	r1, 211
+st	r1, 211
+st	r1, 211
+st	r1, 211
+st	r1, 211
+st	r1, 211
+st	r1, 211
+st	r1, 211
+st	r1, 211
+st	r1, 211
+st	r1, 211
+st	r1, 211
+st	r1, 211
+live	%1
+live	%1
+live	%1
+live	%1
+live	%1
+live	%1
+live	%1
+live	%1
+live	%1
+live	%1
+live	%1
+live	%1
+live	%1
+live	%1
+live	%1
+live	%1
+live	%1
+live	%1
+live	%1
+live	%1
+live	%1
+live	%1
+live	%1
+live	%1
+live	%1
+live	%1
+live	%1
+live	%1
+live	%1
+live	%1
+live	%1
+live	%1
+live	%1
+live	%1
+live	%1
+live	%1
+live	%1
+live	%1
+live	%1
+live	%1
+live	%1
+live	%1
+zjmp	%-210
--- resources/champions/examples/zork.s
+++ resources/champions/examples/zork.s
@@ -1,14 +1,9 @@
 .name "zork"
 .comment "I'M ALIIIIVE"
 
-l2:		sti r1, %:live, %1
-#		and r1, %1, r1
-		add r2, r3, r4
+# INDICATED SIZE : 20
 
-live:	live %1
-		zjmp %:live
-
-#1111 1111 1111 1111
-#0000 0000 0000 0001
-
-#0000 0000 0000 0001
+sti	r1, %12, %1
+add	r2, r3, r4
+live	%1
+zjmp	%-5
